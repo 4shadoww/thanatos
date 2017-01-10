9294c276a08330e0f4a81eb7d3c53bc73e944d61
4shadoww <4shadoww0@gmail.com>
2017-01-10 18:31:37 +0200

    initial commit

---
+++ thanatos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Import python modules
+import sys
+
+
+# Append lib path
+sys.path.append("core/lib")
+
+# Import pywikibot
+
+import pywikibot
+from pywikibot import pagegenerators
+
+# Import core modules
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
